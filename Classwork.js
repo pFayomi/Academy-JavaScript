2be9d94985d611de028f1d4738771df936784a91
Oluwapelumi Fayomi <opelumifayomi@gmail.com>
2026-08-18 11:58:26 +0100

    My new Changes

---
+++ Classwork.js
@@ -0,0 +1,19 @@
+let userName = prompt("Who's there?", '');
+let pass = prompt('Password?', '');
+
+if (userName === 'Admin') {
+  
+  if (pass === 'TheMaster') {
+    console.log('Welcome!');
+  } else if (pass === '' || pass === null) {
+    console.log('Canceled.');
+  } else {
+    console.log('Wrong password');
+  }
+
+} else if (userName === '' || userName === null) {
+  console.log('Canceled.');
+} else {
+  console.log("I don't know you");
+}
+// console.log("git update");
